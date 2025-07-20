51db85d148c891640f32471828fc80e76108cba9
toluwalase oloba <100348104+DaraDjango12@users.noreply.github.com>
2025-07-20 23:18:04 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
+ #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map  # Ensure utils.py is in your PYTHONPATH or same directory
+from utils import access_nested_map  # This must be defined in your utils.py file
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
@@ -10,3 +11,6 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+if __name__ == "__main__":
+    unittest.main()
